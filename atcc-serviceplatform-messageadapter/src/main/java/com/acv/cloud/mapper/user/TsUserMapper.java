a95acf4e3fc4995168e33afb261c15b04485984c
65499 <654998538@qq.com>
2019-05-14 09:45:41 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	atcc-serviceplatform-caradapter/src/main/java/com/acv/cloud/Application.java
    #	atcc-serviceplatform-caradapter/src/main/java/com/acv/cloud/models/remotecallback/RemoteCallBack.java
    #	atcc-serviceplatform-caradapter/src/main/java/com/acv/cloud/services/callback/impl/RemoteCallBackServiceImpl.java
    #	atcc-serviceplatform-caradapter/src/main/java/com/acv/cloud/services/vehicleState/impl/VehicleStateServiceImpl.java

--- atcc-serviceplatform-messageadapter/src/main/java/com/acv/cloud/mapper/user/TsUserMapper.java
+++ atcc-serviceplatform-messageadapter/src/main/java/com/acv/cloud/mapper/user/TsUserMapper.java
@@ -7,12 +7,11 @@ import org.apache.ibatis.annotations.Param;
  * Created by liyang on 2019/5/9 14:46.
  */
 public interface TsUserMapper {
-
     /**
      * 根据用户手机号查询用户userId
      *
      * @param phoneNums
      * @return
      */
-    TsUser  findUserId(@Param("phoneNum") String phoneNums);
+    TsUser findUserId(@Param("phoneNum") String phoneNums);
 }
