d31663b42442a5c1a83643167629e86b070dbbf6
65499 <654998538@qq.com>
2019-05-14 09:36:18 +0800

    2019-05-12,李洋 commit

---
+++ atcc-serviceplatform-messageadapter/src/main/java/com/acv/cloud/mapper/user/TsUserMapper.java
@@ -0,0 +1,18 @@
+package com.acv.cloud.mapper.user;
+
+import com.acv.cloud.models.sys.TsUser;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * Created by liyang on 2019/5/9 14:46.
+ */
+public interface TsUserMapper {
+
+    /**
+     * 根据用户手机号查询用户userId
+     *
+     * @param phoneNums
+     * @return
+     */
+    TsUser findUserId(@Param("phoneNum") String phoneNums);
+}
